839de4803609a129b34886e4c27cf91e2fbb3aa5
k.soshin <k.soshin@partner.samsung.com>
2021-07-07 15:11:07 +0000

    ENH: link ThinObject5K dataset in sec script

--- run_cluster_train.sh
+++ run_cluster_train.sh
@@ -1,5 +1,6 @@
 pip3 install -r requirements.txt
 ln -s /group-volume/orc_srr/k.soshin/data/datasets/COCO+LVIS datasets/COCO+LVIS
+ln -s /group-volume/orc_srr/k.soshin/data/datasets/ThinObject5K datasets/ThinObject5K
 ln -s /group-volume/orc_srr/k.soshin/pretrained_models/  pretrained_models
 ln -s /group-volume/orc_srr/k.soshin/experiments/ experiments
 # python3 train.py models/iter_mask/hrnet18_cocolvis_itermask_3p.py --gpus=0,1,2,3 --workers=12 --exp-name=first-try
